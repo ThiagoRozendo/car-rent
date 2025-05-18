7fd7a6be141e2c5270b9ae418d79e1c38f96f387
JoaoV-AraujoS <joao.araujos@ufrpe.br>
2025-05-18 15:22:34 -0300

    Dados do carro e gets e sets

---
+++ src/main/java/br/ufrpe/negocio/beans/Carro.java
@@ -0,0 +1,67 @@
+package br.ufrpe.negocio.beans;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+    private String placa; //PK
+    private String categoria;
+    private int quantidadeDisponivel;
+    private boolean status;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getQuantidadeDisponivel() {
+        return quantidadeDisponivel;
+    }
+
+    public void setQuantidadeDisponivel(int quantidadeDisponivel) {
+        this.quantidadeDisponivel = quantidadeDisponivel;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
