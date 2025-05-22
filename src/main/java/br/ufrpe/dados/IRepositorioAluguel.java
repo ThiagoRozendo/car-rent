bc5527c90aacc1449f3f710a03a16b8ad4de7b4e
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-05-22 00:26:02 -0300

    feat: corpo dos metodos do repositorio de alugueis
    Também foi trocada a abordagem: de array para array list. Além disso, houve a decisão de implementar o método de finalizar aluguel apenas após a finalização do CRUD. Também foi trocado o tipo de retorno da função excluir aluguel para um boolean, dessa forma é fácil saber se a exclusão foi bem sucedida ou não. Isto pode ser substituído por uma exceção no futuro, assim como nos outros métodos do repositório.

--- src/main/java/br/ufrpe/dados/IRepositorioAluguel.java
+++ src/main/java/br/ufrpe/dados/IRepositorioAluguel.java
@@ -1,6 +1,7 @@
 package br.ufrpe.dados;
 
 import br.ufrpe.negocio.beans.Aluguel;
+import java.util.ArrayList;
 
 import java.time.LocalDate;
 
@@ -8,8 +9,8 @@ public interface IRepositorioAluguel {
 
     void cadastrarAluguel(LocalDate dataInicio, LocalDate dataFim, String placaCarro, String cpfCliente);
     void editarAluguel(int idAluguel, LocalDate dataInicio, LocalDate dataFim, String placaCarro, String cpfCliente);
-    void excluirAluguel(int idAluguel);
-    void finalizarAluguel(int idAluguel);
-    void buscarAluguelPorId(int idAluguel);
-    Aluguel[] listarAlugueis(); //implementacao inicial com array
+    boolean excluirAluguel(int idAluguel); //o retorno foi trocado para boolean para saber se a exclusao foi feita ou nao
+    //void finalizarAluguel(int idAluguel); //metodo sera implementado apos a finalizacao do crud
+    Aluguel buscarAluguelPorId(int idAluguel); //Deve ser do tipo Aluguel
+    ArrayList<Aluguel> listarAlugueis(); //implementacao com arraylist
 }
--- src/main/java/br/ufrpe/dados/RepositorioAluguel.java
+++ src/main/java/br/ufrpe/dados/RepositorioAluguel.java
@@ -4,13 +4,16 @@ import br.ufrpe.negocio.beans.Aluguel;
 
 import java.time.LocalDate;
 
-public class RepositorioAluguel implements IRepositorioAluguel {
-    private Aluguel[] alugueis;
+import java.util.ArrayList;
 
+public class RepositorioAluguel implements IRepositorioAluguel {
+
+    private ArrayList<Aluguel>  alugueis;
     private static RepositorioAluguel instance;
+    private int nextId = 0;
 
     public RepositorioAluguel() {
-        alugueis = new Aluguel[10000];
+        alugueis = new ArrayList<>();
     }
 
     public static RepositorioAluguel getInstance() {
@@ -22,31 +25,49 @@ public class RepositorioAluguel implements IRepositorioAluguel {
 
     @Override
     public void cadastrarAluguel(LocalDate dataInicio, LocalDate dataFim, String placaCarro, String cpfCliente){
-
+        //usar o nextId evita duplicacoes de id, pois quando um aluguel for excluido, o alugueis.sizew() tambem diminui, é mais simples também
+        Aluguel novoAluguel = new Aluguel(nextId++, dataInicio, dataFim, placaCarro, cpfCliente);
+        this.alugueis.add(novoAluguel);
     }
 
     @Override
     public void editarAluguel(int idAluguel, LocalDate dataInicio, LocalDate dataFim, String placaCarro, String cpfCliente){
-
+        Aluguel aluguel = buscarAluguelPorId(idAluguel);
+        if (aluguel != null){
+            aluguel.setDataInicio(dataInicio);
+            aluguel.setDataFim(dataFim);
+            aluguel.setPlacaCarro(placaCarro);
+            aluguel.setCpfCliente(cpfCliente);
+        }
     }
 
     @Override
-    public void excluirAluguel(int idAluguel){
-
+    public boolean excluirAluguel(int idAluguel){ //o retorno foi trocado para boolean para saber se a exclusao foi feita ou nao
+        Aluguel aluguel = buscarAluguelPorId(idAluguel);
+        if (aluguel != null){
+            this.alugueis.remove(aluguel);
+            return true;
+        }
+        return false;
     }
 
-    @Override
+    /*/@Override //metodo sera implementado apos a finalização do crud
     public void finalizarAluguel(int idAluguel){
 
+    }/*/
+
+    @Override
+    public Aluguel buscarAluguelPorId(int idAluguel) {
+        for (Aluguel aluguel : alugueis){
+            if(aluguel.getIdAluguel() == idAluguel){
+                return aluguel;
+            }
+        }
+        return null;
     }
 
     @Override
-    public void buscarAluguelPorId(int idAluguel){
-
-    }
-
-    @Override
-    public Aluguel[] listarAlugueis(){
+    public ArrayList<Aluguel> listarAlugueis(){
         return alugueis;
     }
 }
