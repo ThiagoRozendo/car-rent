de2c304a63e85aa787bfb405cabb684dc30e1192
Miguel Cavalcanti <miguelcavalcanti2006@gmail.com>
2025-07-08 16:35:18 -0300

    refactor: Atualização das exceções

--- src/main/java/br/ufrpe/negocio/excecoes/ClienteNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package br.ufrpe.negocio.excecoes;
-
-public class ClienteNaoEncontradoException extends Exception {
-    public ClienteNaoEncontradoException(String cpf) {
-        super("Não foi encontrado um cliente com o CPF: " + cpf);
-    }
-}
